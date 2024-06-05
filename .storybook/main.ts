8139d719e00d924ca20a712e49df593067021667
Davy De Pauw <github@davydepauw.be>
2024-06-05 12:51:40 +0200

    Provide static path

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,30 +1,34 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
-  // Required
-  framework: '@storybook/react-webpack5',
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  // Optional
-  addons: [
-	"@storybook/addon-webpack5-compiler-swc",
-	"@storybook/addon-onboarding",
-	"@storybook/addon-links",
-	"@storybook/addon-essentials",
-	"@chromatic-com/storybook",
-	"@storybook/addon-interactions",
+  	// Required
+  	framework: '@storybook/react-webpack5',
+  	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  	staticDirs: [
+		{ from: '../dist/assets', to: '/assets' },
+		{ from: '../dist/css', to: '/css' }
 	],
-  docs: {
-    autodocs: 'tag',
-  },
-  swc: () => ({
-    jsc: {
-      transform: {
-        react: {
-          runtime: "automatic",
-        },
-      },
-    },
-  }),
+	// Optional
+  	addons: [
+		"@storybook/addon-webpack5-compiler-swc",
+		"@storybook/addon-onboarding",
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@chromatic-com/storybook",
+		"@storybook/addon-interactions",
+	],
+  	docs: {
+    	autodocs: 'tag',
+  	},
+  	swc: () => ({
+    	jsc: {
+      		transform: {
+        		react: {
+          			runtime: "automatic",
+        		},
+      		},
+	    },
+  	}),
 };
 
 export default config;
