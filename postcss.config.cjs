d3a82e106130da6efb4ca8bab6011f026720fa90
Davy De Pauw <github@davydepauw.be>
2024-07-04 15:26:11 +0200

    Remove sass dependency in favor of postcss

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,12 +1,12 @@
 // postcss.config.js
 module.exports = {
-    parser: 'postcss-scss',
-    plugins: [
-        require('@csstools/postcss-sass'),
-        require('postcss-custom-media'),
-        require('postcss-easy-import')({
-            prefix: "_"
-        }),
-        require('postcss-merge-rules'),
-    ]
-}
+	parser: "postcss-scss",
+	plugins: [
+		require("postcss-easy-import")({
+			prefix: "_",
+		}),
+		require("postcss-import"),
+		require("postcss-custom-media"),
+		require("postcss-merge-rules"),
+	],
+};
