550c072dbb5225c48d25130e69a32d97564fc447
brechtDR <deruytebrecht@gmail.com>
2024-10-02 16:39:10 +0200

    add docu

--- src/stories/templates/breakout-grid/breakoutGrid.stories.tsx
+++ src/stories/templates/breakout-grid/breakoutGrid.stories.tsx
@@ -10,13 +10,19 @@ export type PropsType = HTMLAttributes<HTMLElement>;
 export const Grid: Story = {
 	render: (props: PropsType) => (
 		<div className="page-layout">
-			<div className="full" style={{ backgroundColor: '#ddd', padding: '10px' }}>
+			<div className="grid-full" style={{ backgroundColor: '#ddd', padding: '10px' }}>
 				<strong>FULL TRACK</strong>
 				<span> (1fr)</span>
 			</div>
-			<div className="content" style={{ backgroundColor: '#ddd', padding: '10px' }}>
-				<strong>CONTENT TRACK</strong>
+			<div className="grid-popout" style={{ backgroundColor: '#ddd', padding: '10px' }}>
+				<strong>POPOUT TRACK</strong>
 				<span> (uses container max width)</span>
+			</div>
+			<div className="grid-content" style={{ backgroundColor: '#ddd', padding: '10px' }}>
+				<strong>CONTENT TRACK</strong>
+			</div>
+			<div className="grid-popout-start grid-full-end" style={{ backgroundColor: '#ddd', padding: '10px' }}>
+				<strong>POPOUT TRACK START and FULL END</strong>
 			</div>
 		</div>
 	),
