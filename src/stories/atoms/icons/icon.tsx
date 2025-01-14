4d656d4e82e76557ce6bbdb8037901101a329c47
Davy De Pauw <github@davydepauw.be>
2025-01-14 13:24:18 +0100

    Remove unused CSS file and update prettier config

--- src/stories/atoms/icons/icon.tsx
+++ src/stories/atoms/icons/icon.tsx
@@ -1,51 +1,50 @@
-import { cva, type VariantProps } from 'class-variance-authority';
-import { memo, type ComponentPropsWithRef } from 'react';
-import { type ArrayValue } from '../../../js/types';
-import { cn } from '../../../js/utils/style';
-import styles from './icon.module.css';
+import { cva, type VariantProps } from "class-variance-authority";
+import { memo, type ComponentPropsWithRef } from "react";
+import { type ArrayValue } from "../../../js/types";
+import styles from "./icon.module.css";
 
 export const ICON_NAMES = [
-	'add',
-	'arrow-right',
-	'arrow-up',
-	'arrow-down',
-	'calendar',
-	'chevron-left',
-	'chevron-right',
-	'chevron-up',
-	'chevron-down',
-	'company',
-	'clear',
-	'close',
-	'delete',
-	'download',
-	'error',
-	'filter',
-	'info',
-	'menu',
-	'facebook',
-	'twitter',
-	'instagram',
-	'user',
-	'external',
+	"add",
+	"arrow-right",
+	"arrow-up",
+	"arrow-down",
+	"calendar",
+	"chevron-left",
+	"chevron-right",
+	"chevron-up",
+	"chevron-down",
+	"company",
+	"clear",
+	"close",
+	"delete",
+	"download",
+	"error",
+	"filter",
+	"info",
+	"menu",
+	"facebook",
+	"twitter",
+	"instagram",
+	"user",
+	"external",
 ] as const;
 
 const iconVariants = cva(styles.icon, {
 	variants: {
 		size: {
-			sm: styles['icon--sm'],
-			md: styles['icon--md'],
-			lg: styles['icon--lg'],
-			xl: styles['icon--xl'],
+			sm: styles["icon--sm"],
+			md: styles["icon--md"],
+			lg: styles["icon--lg"],
+			xl: styles["icon--xl"],
 		},
 	},
 	defaultVariants: {
-		size: 'md',
+		size: "md",
 	},
 });
 
 export type IconName = ArrayValue<typeof ICON_NAMES>;
-export type IconProps = ComponentPropsWithRef<'svg'>;
+export type IconProps = ComponentPropsWithRef<"svg">;
 export type IconVariants = VariantProps<typeof iconVariants>;
 
 export type Props = IconProps & IconVariants & { variant: IconName };
